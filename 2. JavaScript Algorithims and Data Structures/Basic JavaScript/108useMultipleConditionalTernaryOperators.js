4501c74b0d19ba83484a4a3b2c38a333edef4f75
Whamu <kimwoodfield@live.com>
2020-09-10 21:40:32 +1000

    another challenge completed

--- 2. JavaScript Algorithims and Data Structures/Basic JavaScript/108useMultipleConditionalTernaryOperators.js
+++ 2. JavaScript Algorithims and Data Structures/Basic JavaScript/108useMultipleConditionalTernaryOperators.js
@@ -1,7 +1,11 @@
 // In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return "positive", "negative" or "zero".
 
-function checkSign(num) {
+function checkSign(num) { // function created
+
+    return (num > 0) ? "positive" // is num more than 0? return positive if so.
+    : (num < 0) ? "negative" // else if number less than 0? return negative if so.
+    : "zero" // else if number is 0 return zero.
 
 }
 
-checkSign(10);
+checkSign(10); // function call
