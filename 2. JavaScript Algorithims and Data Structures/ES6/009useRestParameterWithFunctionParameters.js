1897893f419a97d559c0c455c7392132c74ec69f
Whamu <kimwoodfield@live.com>
2020-09-15 22:13:55 +1000

    updates

--- 2. JavaScript Algorithims and Data Structures/ES6/009useRestParameterWithFunctionParameters.js
+++ 2. JavaScript Algorithims and Data Structures/ES6/009useRestParameterWithFunctionParameters.js
@@ -1,6 +1,6 @@
 // Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
 
-const sum = (...args) => {
-  return args.reduce((a, b) => a + b, 0);
+const sum = (...args) => { // constant variable that stores a function that takes values
+  return args.reduce((a, b) => a + b, 0); //
 }
   
