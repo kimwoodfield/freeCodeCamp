7285ffcd2dfc82e9adf16f73191608d3e1778edf
Whamu <kimwoodfield@live.com>
2020-09-17 20:29:50 +1000

    updates

--- 2. JavaScript Algorithims and Data Structures/ES6/015useDestructuringAssignWithRestParamToReassignArrElements.js
+++ 2. JavaScript Algorithims and Data Structures/ES6/015useDestructuringAssignWithRestParamToReassignArrElements.js
@@ -1,2 +1,11 @@
 // Use destructuring assignment with the rest parameter to perform an effective Array.prototype.slice() so that arr is a sub-array of the original array source with the first two elements omitted.
 
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // Only change code below this line
+  const [a,b,...arr] = list; // Change this line
+  // Only change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
