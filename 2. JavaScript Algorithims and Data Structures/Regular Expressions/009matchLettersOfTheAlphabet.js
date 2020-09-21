61eba223eefce29b39b1badcae4f0879c7697af6
Whamu <kimwoodfield@live.com>
2020-09-21 10:34:23 +1000

    updates:more challenges completed

---
+++ 2. JavaScript Algorithims and Data Structures/Regular Expressions/009matchLettersOfTheAlphabet.js
@@ -0,0 +1,8 @@
+/* Match all the letters in the string quoteSample.
+
+Note
+Be sure to match both upper- and lowercase letters. */
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
