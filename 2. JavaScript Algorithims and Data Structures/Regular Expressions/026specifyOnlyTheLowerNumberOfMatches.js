d8314a955dc8f01bfdc8e323ac9309a86d216993
kimwoodfield <kimwoodfield@live.com>
2020-09-22 14:05:48 +1000

    updates: more challenges completed

---
+++ 2. JavaScript Algorithims and Data Structures/Regular Expressions/026specifyOnlyTheLowerNumberOfMatches.js
@@ -0,0 +1,6 @@
+// Change the regex haRegex to match the word "Hazzah" only when it has four or more letter z's.
+
+let haStr = "Hazzzzah";
+let haRegex = /^Haz{4,}ah$/ig; // Change this line
+let result = haRegex.test(haStr);
+
