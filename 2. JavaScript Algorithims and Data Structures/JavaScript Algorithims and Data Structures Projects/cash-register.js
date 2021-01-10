605cfd42bae569bf20f64c49b8e92e77f0a9bd27
kimwoodfield <kimwoodfield@live.com>
2021-01-10 11:21:32 +1000

    Finished JavaScript Algorithims and Data Structures

--- 2. JavaScript Algorithims and Data Structures/JavaScript Algorithims and Data Structures Projects/cash-register.js
+++ 2. JavaScript Algorithims and Data Structures/JavaScript Algorithims and Data Structures Projects/cash-register.js
@@ -1,5 +1,5 @@
 function checkCashRegister(price, cash, cid) {
-    
+
     // Amount owed (multiplied by 100)
     let amountOwed = (cash - price) * 100;
     console.log(amountOwed);
@@ -20,7 +20,7 @@ function checkCashRegister(price, cash, cid) {
         let amount = 0;
         let value = denominations[i];
 
-        // While the amount owed is more than the value of the bill AND while the available bill has more than what it is worth, deduct the value of the bill from each one we deduct. Deduct the value of the bill from the amount we owe the customer and add the value of the bill to the amount we will return to the customer.
+        // Basically, as long as what we owe is greater than the value of the current bill and while there are bills available to take, reduce how many available bills we have by what they are worth, decrease what we owe by the same value and add the value of the bill to the amount we can return.
         while (amountOwed >= value && availableBills >= value) {
             availableBills -= value;
             amountOwed -= value;
@@ -30,7 +30,7 @@ function checkCashRegister(price, cash, cid) {
         console.log(amountOwed);
         console.log(amount);
 
-        // If there are no available bills then the register is closed.
+        // If there are no available bills, change the register to closed.
         if (availableBills == 0) {
             answer.status = "CLOSED";
         }
@@ -40,18 +40,18 @@ function checkCashRegister(price, cash, cid) {
             answer.status = "OPEN";
         };
 
-        // If the amount we will return to the customer is more than 0, divide it by 100 to return it to its original value and push it into the object we are returning.
+        // If the amount we will return to the customer is more than 0 during the current iteration, add the value of the current bill that we can return to the running total of that bill we will be returning.
         if (amount > 0) {
             answer.change.push([currentBill[0], amount / 100]);
-        } 
+        }
     }
     // If the amount we owe is more than 0, it means we've looped through all of our availableBills and can't make up the total, so return insufficient funds.
     if (amountOwed > 0) {
         answer.status = "INSUFFICIENT_FUNDS";
         answer.change = [];
         return answer;
-    } 
-    // Otherwise, if the status of the object was changed to closed, replace the array in 'change' with the cid array.
+    }
+    // Otherwise, if the status of the object was changed to closed but what we owe equals 0 it means that we had just enough change in the register to return to the customer so return the cid array.
     else if (answer.status == "CLOSED") {
         answer.change = cid;
     }
@@ -59,5 +59,15 @@ function checkCashRegister(price, cash, cid) {
 
 
 }
-  
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+]);
